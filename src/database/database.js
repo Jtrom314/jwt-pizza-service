5c7082662bb45c375a0950ba46b3e8768a21400a
Jtrom314 <97546727+Jtrom314@users.noreply.github.com>
2024-08-12 19:19:39 -0600

    D9 db logging

--- src/database/database.js
+++ src/database/database.js
@@ -4,6 +4,7 @@ const config = require('../config.js');
 const { StatusCodeError } = require('../endpointHelper.js');
 const { Role } = require('../model/model.js');
 const dbModel = require('./dbModel.js');
+const Logger = require('../logger.js')
 class DB {
   constructor() {
     this.initialized = this.initializeDatabase();
@@ -284,6 +285,7 @@ class DB {
   }
 
   async query(connection, sql, params) {
+    Logger.databaseLogger(sql)
     const [results] = await connection.execute(sql, params);
     return results;
   }
